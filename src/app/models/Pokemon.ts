4574334e7fdc392eebbd0870514d7c9742b0f95d
Marcio Souza <ciorma7@hotmail.com>
2024-10-17 17:37:51 -0300

    #task_poke_detail - Adicionando mais informações de detalhe

--- src/app/models/Pokemon.ts
+++ src/app/models/Pokemon.ts
@@ -1,6 +1,11 @@
 export interface IPokemon {
+    id: string | null;
     name: string | null;
+    types: Array<IPokemonType> | null;
+    height: string | null;
+    weight: string | null;
     urlImage: string | null;
+    abilities: Array<IAbility> | null;
     stats: Array<IPokemonStats> | null;
 }
 
@@ -13,4 +18,12 @@ export interface IPokemonStats {
 export interface IStats {
     name: string;
     url: string;
+}
+
+export interface IAbility {
+    ability: any;
+}
+
+export interface IPokemonType {
+    type: any;
 }
