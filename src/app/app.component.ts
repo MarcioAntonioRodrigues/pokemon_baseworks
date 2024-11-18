54c65a44152b6d40c55ff805bfb4eefcd591d686
Marcio Souza <ciorma7@hotmail.com>
2024-11-18 16:19:08 -0300

    #task_poke_layout - Alterações de layout no menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HttpClientModule } from "@angular/common/http";
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { PokemonService } from "./services/pokemonService";
 
 import { HomeComponent } from "./components/home/home.component";
 import { PokemonDetailComponent } from "./components/pokemon-detail/pokemon-detail.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
 	selector: "app-root",
@@ -17,6 +20,9 @@ import { HeaderComponent } from "./components/header/header.component";
 		HeaderComponent,
 		PokemonDetailComponent,
 		HttpClientModule,
+		CommonModule,
+		MatIconModule,
+		MatSidenavModule
 	],
 	providers: [
 		PokemonService,
@@ -26,4 +32,19 @@ import { HeaderComponent } from "./components/header/header.component";
 })
 export class AppComponent {
 	title = "pokemonProject";
+	sideNavWidth = "250px";
+	searchNavWidth = "0px"
+	isSideNavOpen = false;
+
+	public openNav() {
+		this.isSideNavOpen = !this.isSideNavOpen;
+		if (!this.isSideNavOpen) {
+			this.sideNavWidth = "250px";
+			this.searchNavWidth = "0px";
+		}
+		else {
+			this.sideNavWidth = "100px";
+			this.searchNavWidth = "250px";
+		}
+	}
 }
